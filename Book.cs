12814de29b100a1da8d602cba8f177ce68917688
Daniel Ramirez <danielramire1996@gmail.com>
2023-05-09 22:11:27 -0500

    First commit

---
+++ Book.cs
@@ -0,0 +1,16 @@
+public class Book
+{
+    public string Title{get;set;}
+    
+    public int PageCount{get;set;}
+
+    public string Status{get;set;}
+
+    public DateTime publishedDate{get;set;}
+
+    public string[] Authors{get;set;}
+
+    public string[] Categories{get;set;}
+
+
+}
