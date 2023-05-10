c82745dc21516a65a6f9d7ca28b4e841454030f2
Daniel Ramirez <danielramire1996@gmail.com>
2023-05-09 22:50:42 -0500

    Implement book list

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,7 +1,19 @@
 public class LinqQueries
 {
+    private List<Book> bookCollection = new List<Book>();
     public LinqQueries()
     {
+        using(StreamReader reader = new StreamReader("books.json"))
         
+        {
+            string json = reader.ReadToEnd();
+            this.bookCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions(){PropertyNameCaseInsensitive = true});
+
+        }
+
+    }
+    public IEnumerable<Book> allCollection()
+    {
+        return bookCollection;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinqQueries queries = new LinqQueries();
+
+
+
+PrintValues(queries.allCollection());
+
+
+
+void PrintValues(IEnumerable<Book> bookList)
+{
+    Console.WriteLine("{0, -60 } {1,15} {2,15}\n", "Title", "Pages", "Plublished date");
+    foreach(var item in bookList)
+    {
+        Console.WriteLine("{0, -60 } {1,15} {2,15}", item.Title, item.PageCount, item.publishedDate.ToShortDateString());
+    }
+}
