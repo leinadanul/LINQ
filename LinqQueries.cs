1eeda23e66332217217be08540be8644426b2348
Daniel Ramirez <danielramire1996@gmail.com>
2023-05-13 18:40:21 -0500

    class 15

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -16,4 +16,54 @@ public class LinqQueries
     {
         return bookCollection;
     }
+
+    public IEnumerable<Book> BookAfter2000()
+    {   // extension method 
+        //return bookCollection.Where(p=> p.publishedDate.Year > 2000);
+
+        return from l in bookCollection where l.publishedDate.Year > 2007 select l;
+    }
+
+    public IEnumerable<Book> bookMore250PagesAndWordsWithActtion()
+    {
+        // extension method 
+        //return bookCollection.Where(p => p.PageCount > 250 && p.Title.Contains ("in Action"));
+
+
+        //Query expression
+        return from l in bookCollection where l.PageCount > 250 && l.Title.Contains("in Action") select l;
+    }
+
+    public bool allBooksWithStatus()
+    {
+        return bookCollection.All(p => p.Status != string.Empty);
+    }
+
+    public bool anyBookWasPublishedIn2005()
+    {
+        return bookCollection.Any( p => p.publishedDate.Year == 2005);
+    }
+
+    public IEnumerable<Book> bookThatsConteinTheWordPython()
+    {
+        return bookCollection.Where(p => p.Categories.Contains("Python"));
+    }
+
+    public IEnumerable<Book> bookThatsConteinTheWordJavaWithAscName()
+    {
+        return bookCollection.Where(p => p.Categories.Contains("Java")).OrderBy( p => p.Title);
+    }
+
+    public IEnumerable<Book> bookMore450PagesOrderByDesc()
+    {
+        // extension method 
+        return bookCollection.Where(p => p.PageCount > 450).OrderByDescending(p => p.PageCount);
+
+    }
+
+
+
+
+
+
 }
--- Program.cs
+++ Program.cs
@@ -2,9 +2,32 @@
 
 
 
-PrintValues(queries.allCollection());
+
+// print all collection 
+//PrintValues(queries.allCollection());
+
+// print book's after 2000s
+//PrintValues(queries.BookAfter2000());
 
 
+// All books have status
+//Console.WriteLine($" All books have status? - {queries.allBooksWithStatus()}");
+
+
+// some book was published in 2005
+//Console.WriteLine($" Any book was published in 2005? - {queries.anyBookWasPublishedIn2005()}");
+
+//Python books
+//PrintValues(queries.bookThatsConteinTheWordPython());
+
+// print books with more 250 pages and the word "in action "
+//PrintValues(queries.bookMore250PagesAndWordsWithActtion());
+
+//Java books order by name 
+//PrintValues(queries.bookThatsConteinTheWordJavaWithAscName());
+
+//Books with more 450 pages oreden by descendent 
+PrintValues(queries.bookMore450PagesOrderByDesc());
 
 void PrintValues(IEnumerable<Book> bookList)
 {
